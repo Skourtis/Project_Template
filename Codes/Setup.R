5d759ea8a286009d1709e5b9b1c75d012f546eb8
Skourtis <51754041+Skourtis@users.noreply.github.com>
2021-02-15 21:09:34 +0100

    Piggi_Ignore Files

--- Codes/Setup.R
+++ Codes/Setup.R
@@ -1,9 +1,12 @@
 #Settting up directory
-install.packages("tidyverse")
-pacman::p_load(piggyback, renv, here, )
-renv::restore()
-piggyback::pb_track(here::here("Datasets")) %>%
-    pb_upload()
+#install.packages("tidyverse")
+pacman::p_load(piggyback, renv, here, tidyverse )
+#renv::restore()
+
+## Created a first release on Github
+#pb_new_release("Skourtis/Project_Template")
+piggyback::pb_track("*.zip") %>%
+    pb_upload(repo = "Skourtis/Project_Template")
 
 ##end
 renv::snapshot()
