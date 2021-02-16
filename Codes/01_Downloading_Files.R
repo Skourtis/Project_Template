b1aea23a3485d97247bd8c2c40c56d7369e7d495
Skourtis <51754041+Skourtis@users.noreply.github.com>
2021-02-16 18:42:16 +0100

    Adding the installation of Proteus

--- Codes/01_Downloading_Files.R
+++ Codes/01_Downloading_Files.R
@@ -1,6 +1,7 @@
 #Downloading from Git template
 options(repos = getOption("repos")["CRAN"])
 renv::restore()
+devtools::install_github("bartongroup/Proteus", build_opts= c("--no-resave-data", "--no-manual"), build_vignettes=FALSE)
 pacman::p_load(piggyback, renv, here, tidyverse)
 
 pb_download()
