1c33f84232081e4a8b8fe0307418d88907923338
Skourtis <51754041+Skourtis@users.noreply.github.com>
2021-02-16 11:57:36 +0100

    targets

---
+++ _targets.R
@@ -0,0 +1,19 @@
+library(targets)
+library(tarchetypes)
+source()
+options(tidyverse.quiet = TRUE)
+
+
+### _targets.R
+
+tar_pipeline(
+    tar_target( ### first target 
+        
+    ),
+    tar_target( ### second target
+        ,
+    )
+)
+
+#run tar_visnetwork()
+#tar_mare()
