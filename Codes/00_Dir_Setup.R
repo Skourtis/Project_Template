518b32223187342a83ee8b3113894a5c34d2ac5c
Skourtis <51754041+Skourtis@users.noreply.github.com>
2021-02-17 09:56:37 +0100

    Added Rdata to piggyback and gitignore

--- Codes/00_Dir_Setup.R
+++ Codes/00_Dir_Setup.R
@@ -6,7 +6,7 @@ pacman::p_load(piggyback, renv, here, tidyverse )
 
 ## Created a first release directly on Github
 #pb_new_release("Skourtis/Project_Template")
-piggyback::pb_track(c("*.zip","*.dat","*,RData")) %>%
+piggyback::pb_track(c("*.zip","*.dat","*.RData")) %>%
     pb_upload(repo = "Skourtis/Project_Template")
 
 ##end
